c3cfc6a74b17337fa2393e330295f142ab39a1ac
Deepak Kumar <deepak.hcu07@gmail.com>
2020-04-10 18:43:57 +0800

    Added example codes for control structure

---
+++ control-structure/for-loop.py
@@ -0,0 +1,16 @@
+# Program to iterate over the list
+
+# Creating a list
+cars = ["Honda", "Huendai", "Kia","Toyota"]
+
+# Creating a Set
+# cars = {"BMW","Mercedes","Audi","Tata","Ford"}
+# Created a tuple.
+# Tuple is immutable. We cannot change the Tuple
+# cars = ("Toyota","BMW","Honda","Tata", "Mercedes","Audi")
+
+# Print the type of type
+print(type(cars))
+
+for car in cars :
+    print(car)
---
+++ control-structure/while-loop.py
@@ -0,0 +1,9 @@
+# Program to print the table of a given number
+
+num = 23
+
+i = 1
+
+while i <= 10:
+    print(num * i)
+    i += 1
